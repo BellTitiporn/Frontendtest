34075819d52148152ccb738c9116bb7af4f97048
BellTitiporn <titiporn.wbell@gmail.com>
2024-04-01 16:14:28 +0700

    Update Navbar.tsx

--- animation-button/src/component/Navbar.tsx
+++ animation-button/src/component/Navbar.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import { Select } from 'antd';
-import { useTranslation } from 'react-i18next'; // Importing useTranslation hook
+import { useTranslation } from 'react-i18next'; 
 
 const Navbar: React.FC = () => {
-  const { i18n } = useTranslation(); // Using useTranslation hook to access translation functions
+  const { i18n } = useTranslation(); 
 
-  // Function to handle language change
   const onChange = (value: string) => {
-    i18n.changeLanguage(value); // Change the language when the user selects a new language
+    i18n.changeLanguage(value);
   };
 
-  // Function to handle search
   const onSearch = (value: string) => {
     console.log('search:', value);
   };
 
-  // Filter `option.label` match the user type `input`
   const filterOption = (input: string, option?: { label: string; value: string }) =>
     (option?.label ?? '').toLowerCase().includes(input.toLowerCase());
 
