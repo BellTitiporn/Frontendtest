420082a626e0fa73c8d1a8086d72d1ada4e05422
BellTitiporn <titiporn.wbell@gmail.com>
2024-04-01 11:36:25 +0700

    Create App.tsx

---
+++ animation-button/src/App.tsx
@@ -0,0 +1,24 @@
+import Home from './component/Home'
+import { Route,Routes} from 'react-router-dom'
+import './App.css'
+import Test1 from './component/Layout&Style'
+import i18n from './i18n'
+import { I18nextProvider } from 'react-i18next';
+import FormComponent from './component/Form'
+
+
+function App() {
+
+  return (
+    <I18nextProvider i18n={i18n}>
+      <div>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/layout' element={<Test1></Test1>}></Route>
+          <Route path='/form' element={<FormComponent/>}></Route>
+        </Routes>
+      </div></I18nextProvider>
+    )
+}
+
+export default App
